8d52d2fc62ded4bb2c57c267113d0a7d4be811c8
goldjake-droid <goldjake@umich.edu>
2026-04-10 20:06:03 -0400

    Delete geoscope directory

--- geoscope/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import dynamic from 'next/dynamic';
-
-const GeoMap = dynamic(() => import('@/components/GeoMap'), { ssr: false });
-
-export default function Home() {
-  return <GeoMap />;
-}
